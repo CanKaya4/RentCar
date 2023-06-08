ca3159502d5908848ed4392cd68f85869f5a7ea0
90534 <90534@CAN>
2023-06-09 01:02:46 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+CarDtoTest();
+
+//ColorTest();
+
+//BrandTest();
+
+//CarTest();
+static void ColorTest()
+{
+    ColorManager colorManager = new ColorManager(new EfColorDal());
+    foreach (var item in colorManager.GetAll())
+    {
+        Console.WriteLine("Renk İsmi \n" + item.ColorName);
+    }
+}
+
+static void BrandTest()
+{
+    BrandManager brandManager = new BrandManager(new EfBrandDal());
+    foreach (var item in brandManager.GetAll())
+    {
+        Console.WriteLine("Marka İsmi \n" + item.BrandName);
+    }
+}
+
+static void CarTest()
+{
+    CarManager carManager = new CarManager(new EfCarDal());
+    foreach (var item in carManager.GetAll())
+    {
+        Console.WriteLine("Araba Açıklaması \n" + item.Description);
+    }
+}
+
+static void CarDtoTest()
+{
+    CarManager carManager = new CarManager(new EfCarDal());
+    foreach (var item in carManager.GetCarDetails())
+    {
+        Console.WriteLine(item.CarId + "||" + item.ColorName);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,28 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarWindContext>, ICarDal
+    {
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (CarWindContext context = new CarWindContext())
+            {
+                var result = from c in context.Cars
+                             join r in context.Colors
+                             on c.CarId equals r.ColorId
+                             select new CarDetailDto { CarId = c.CarId, Description = c.Description, ColorId = r.ColorId, ColorName = r.ColorName };
+                return result.ToList();
+            }
+        }
+    }
+}
