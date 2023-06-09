2645ecddddf9669c45d53be01efd8aaac3e27e4e
90534 <90534@CAN>
2023-06-09 13:49:18 +0300

    Web API Kurulumu

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        public List<Car> GetAll()
+        {
+            return new List<Car>
+            {
+                new Car { CarId = 10, BrandId = 1, Description = "iyi Araba" },
+                new Car { CarId = 11, BrandId = 2, Description = "Kötü Araba" },
+            };
+        }
+    }
+}
